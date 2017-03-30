97288c60d5565214840f7c633a4f214ece54083a
atilaT0R <atilator99@gmail.com>
2017-03-30 21:37:49 +0200

    menu y gmaps
    Con código de @espectalll

---
+++ conocenos.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var position = {lat: 39.8779614, lng: -3.9447705};
+
+var mapOptions = {	
+	center: position,
+	zoom: 15,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var markerOptions = {
+	position: position,
+	map: map,
+	title: "Hydroad"
+};
+
+var market = new google.maps.Marker(markerOptions);
