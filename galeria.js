c705599579f9b37dec03bd45947fa2548db30e22
Francisco Gómez García <espectalll@kydara.com>
2017-04-06 22:43:09 +0200

    Vista previa de galería funcional

--- galeria.js
+++ galeria.js
@@ -1,7 +1,38 @@
 'use strict';
 
+var closePreview = function(bg, pic) {
+	bg.style.cursor = 'default';
+	bg.addEventListener('transitionend', function(e) {
+		this.parentNode.removeChild(this);
+	});
+	bg.style.opacity = '0';
+	pic.style = '';	
+	document.body.style.overflow = '';
+};
+
 for(var item of document.getElementsByClassName('picture')) {
 	item.addEventListener('click', function() {
-		alert(this.getElementsByTagName("img")[0].src);
+		if (document.getElementsByClassName('bg').length == 0) {
+			var pic = this;
+			pic.style.zIndex = 4;
+			pic.style.position = 'fixed';
+			pic.style.top = '5%';
+			pic.style.left = '50%';
+			pic.style.transform = 'translateX(-50%)';
+			pic.style.height = '90%';
+			var bg = document.createElement('div');
+			bg.className = 'bg';
+			document.body.appendChild(bg);
+			document.body.style.overflow = 'hidden';
+			window.getComputedStyle(bg).opacity;
+			bg.style.opacity = 1;
+			bg.addEventListener('click', function(e) {
+				e.preventDefault();
+				closePreview(this, pic);
+			});
+		} else {	
+			bg = document.getElementsByClassName('bg')[0];
+			closePreview(bg, this);
+		};
 	});
 };
